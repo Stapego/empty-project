ac6cdd225a9873f335d6f9072efa316a66f52325
Stapego <staapp@gmail.com>
2025-04-24 22:16:40 +0300

    fds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://stapego.github.io',
-  })
+  site: 'https://stapego.github.io',
+  integrations: [preact()]
+})
